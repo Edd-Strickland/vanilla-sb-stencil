3ece6ac6eac6cbdb17a5d7091e964b026d204084
Edd-Strickland <estrickland@yoobic.com>
2019-10-15 11:19:39 +0100

    feat(storybook): config updated to run locally

--- src/components/html/html.stories.js
+++ src/components/html/html.stories.js
@@ -5,12 +5,4 @@ export default {
     }
   };
 
-  export const defaultComponent = () => {
-    let button = document.createElement('button');
-    button.value = 'Submit';
-    button.name = 'submit_form';
-    button.type = 'submit';
-    return button;
-  };
-
   export const withText = () => '<button class="btn" value="reset" name="reset_form" type="reset">Text</button>';
