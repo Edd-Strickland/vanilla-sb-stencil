3545f197135f980dbf57f71904ec2a73ceb62f70
Edd <estrickland@yoobic.com>
2019-10-28 14:31:37 +0000

    chore(notation): comments added

--- .storybook/config.js
+++ .storybook/config.js
@@ -12,5 +12,9 @@ initDsm({
     addParameters,
     callback: () => configure(loadStories, module)
 });
-
+/**
+ * Required to run locally as well as sending to DSM 
+ * if no local build required comment out. 
+ *  
+ */ 
 configure(req, module);
